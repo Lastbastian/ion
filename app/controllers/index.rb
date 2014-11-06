7cf583da85bd72c0369402cb95c61070df60dea6
Chris Bastian <peacethrubeats@gmail.com>
2014-11-06 11:40:38 -0800

    controller

---
+++ app/controllers/index.rb
@@ -0,0 +1,37 @@
+get '/' do
+
+
+end
+
+
+
+
+
+
+get '/take_survey/:id' do
+
+end
+
+
+
+
+
+
+
+post '/take_survey/:id' do
+
+
+redirect '/'
+end
+
+
+
+
+
+
+
+post '/create_survey/:id' do
+
+
+redirect '/'
+end
