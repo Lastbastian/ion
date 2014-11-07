50694b76d486e8b4fb9decfc09e6d85492dc98f7
Shirley Shaw <shirleyshaw.is@gmail.com>
2014-11-06 16:09:25 -0800

    updated class names in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,12 @@ require 'faker'
 10.times do
   Survey.create(title: Faker::Name.title, description: Faker::Lorem.sentence)
   User.create(username: 'joejoe', email: 'joejoe@joe.com', password_hash: 123456)
-  Surveyquestions.create(content: Faker::Name.title)
-  Surveychoices.create(content: Faker::Name.title)
-  Surveyanswers.create(content: Faker::Name.title)
+  SurveyQuestions.create(content: Faker::Name.title)
+  SurveyChoices.create(content: Faker::Name.title)
+  SurveyAnswers.create(content: Faker::Name.title)
 end
+
+
 
 
 #, vote_count: 0, content_link: Faker::Internet.url)
