566287776dba4e3b4575f5e863d8201b3e163a91
Wesley Gauntt <wesleygauntt92@gmail.com>
2014-11-07 16:33:16 -0800

    I took a machete to the index.

--- app/controllers/cat_controller.rb
+++
@@ -1,4 +0,0 @@
-post '/cats' do
-  new_cat = Cat.create({breed: params[:breed], name:Faker::Name.name})
-  redirect '/'
-end
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,88 +4,49 @@ end
 
 get '/' do
   @surveys = Survey.all
-
   erb :index
 end
 
 get '/create_survey' do
-
   erb :create_survey
 end
 
 post '/surveys/:id/edit' do
   @questions = SurveyQuestion.create(params[:survey_question])
-  p params
-  # @survey_question_id = params[:survey_choice][:survey_question_id]
-
-  p "***********"
-
-
-  p @survey_question_id = SurveyQuestion.last.id
+  @survey_question_id = SurveyQuestion.last.id
   SurveyChoice.create(content: params[:survey_choice1][:content], survey_question_id: "#{@survey_question_id}")
   SurveyChoice.create(content: params[:survey_choice2][:content], survey_question_id: "#{@survey_question_id}")
   SurveyChoice.create(content: params[:survey_choice3][:content], survey_question_id: "#{@survey_question_id}")
   SurveyChoice.create(content: params[:survey_choice4][:content], survey_question_id: "#{@survey_question_id}")
-
-  p "***********"
-
 end
 
 post '/surveys' do
   @survey = Survey.create(params[:survey])
   redirect "/surveys/#{@survey.id}/edit"
-
 end
 
 get '/surveys/:id/edit' do
   @survey = Survey.find(params[:id])
-
   erb :edit_survey
 end
 
 get '/surveys/:id/result' do
   @result = Survey.find(params[:id])
   @answer = SurveyAnswer.where(:question_id, params[:survey_question])
-
   erb :results_survey
-end
-
-post '/surveys/:id/possible_choices' do
-  # p "******"
-  # @survey_id = params[:survey_choice][:survey_id]
-  # @survey_choice = SurveyChoice.create(params[:survey_content])
-  # @surveyquestion_id = SurveyQuestion.find[survey_question_id]
-  p params
-
-
-  redirect "/surveys/#{@survey_id}/edit"
 end
 
 get '/take_survey/:id' do
   @survey = Survey.find(params[:id])
   @question = SurveyQuestion.where(:survey_id => @survey)
   @choice = SurveyChoice.where(:survey_question_id => @question)
-
   erb :take_survey
 end
-
 
 get '/create_survey' do
   erb :create_survey
 end
 
-  # @survey = Survey.new(params[:survey])
-  # if @survey.save
-  #   @question = @survey.SurveyQuestion.new(params[:survey_question])
-  #   if @question.save
-  #     @question.survey_choices.create!(params[:survey_choice])
-  #     redirect '/'
-  #   else
-  #     erb :create_survey
-  #   end
-  # else
-  #   erb :create_survey
-  # end
 get '/user/sign_up' do
   erb :sign_up
 end
